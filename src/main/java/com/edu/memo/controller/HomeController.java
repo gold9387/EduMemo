d7ec508411a422a9d25d724d8587030f8341047f
gold9387 <gold9387@gmail.com>
2023-10-20 01:30:02 +0900

    테스트 코드 작성

--- src/main/java/com/edu/memo/controller/HomeController.java
+++ src/main/java/com/edu/memo/controller/HomeController.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
     
-    @GetMapping("/")
+    @GetMapping("/test1")
     public String home() {
         return "Home Page";
+    }
+
+    @GetMapping("/test2")
+    public String home2() {
+        return "Home Page2";
     }
 }
